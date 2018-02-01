9d7dbb527a72c5b0e431241f3155304342d0a822
rajaneesh <rajaneesh.vommi@gmail.com>
2018-02-01 11:51:21 +0530

    version 0.2

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
+/**
+ * Created by rajan on 31-01-2018.
+ */
 var modal  = document.getElementById("pop-modal");
 var closemodal =document.getElementsByClassName("close")[0];
 
 closemodal.onclick = function(){
-	modal.style.display ="none";
+    modal.style.display ="none";
 }
 
- document.onkeydown = function(p){
-	if(p.keyCode == 27){
-		modal.style.display ="none";
-	}else{
-		modal.style.display ="block";
-	}
+document.onkeydown = function(p){
+    if(p.keyCode == 27){
+        modal.style.display ="none";
+    }else{
+        modal.style.display ="block";
+    }
 }
 
 window.onload =function(){
-	document.onclick =function(c){
-		if(c.target.id !== 'pop-modal'){
-			modal.style.display="none";
-		}
-	}
+    document.onclick =function(c){
+        if(c.target.id == 'pop-modal'){
+            modal.style.display="none";
+        }
+    }
 }
